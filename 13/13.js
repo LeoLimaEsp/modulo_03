e98910cb726e067ad63752c1a5a3056aac5bc40d
leonel <leonel.limae@hotmail.com>
2022-06-11 06:46:47 -0500

    Ejercicio 13 Pilas

---
+++ 13/13.js
@@ -0,0 +1,70 @@
+class Stack {
+
+    constructor() {
+        this.items = [];
+    }
+
+    push(item) {
+        this.items.push(item);
+    }
+    
+    pop() {
+        return this.items.pop();
+    }
+    
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    
+    size() {
+        return this.items.length;
+    }
+    
+    print() {
+        console.log(this.items);
+    }
+}
+
+let stack_contents = new Stack();
+stack_contents.push(3)
+stack_contents.push(2)
+stack_contents.push(3)
+stack_contents.push(4)
+stack_contents.push(6)
+stack_contents.push(8)
+stack_contents.push(1)
+stack_contents.push(2)
+stack_contents.push(5)
+stack_contents.push(5)
+
+function retirar_contenedor_id(stack, id)
+{
+    let auxiliar = []
+
+    let salir = 1
+
+    let cantidad = 0
+
+    do
+    {
+        if (stack.peek() == id)
+        {
+            stack.pop()
+            
+            salir = 0
+        }
+        else
+        {
+            auxiliar.push(stack.pop())
+
+            cantidad++
+        }
+    }
+    while (salir == 1)
+
+    for (i = cantidad -1; i >= 0; i--)
+    {
+        stack.push(auxiliar[i])
+    }
+    return stack.items
+}
