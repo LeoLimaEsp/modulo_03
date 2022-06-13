4b31fc309163429b4529f54b98a27e947e1b0648
leonel <leonel.limae@hotmail.com>
2022-06-13 16:56:40 -0500

    Ejercicio 3

--- 3/03.js
+++ 3/03.js
@@ -3,3 +3,5 @@ const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5/9;
 let grados = prompt("Ingresa los grados fahrenheiteit");
 let resultadoGrados = fahrenheitToCelsius(grados);
 alert("Los grados celsius son: " + resultadoGrados.toFixed(2)) /* para tener dos decimales nada mas */
+
+/* Ejercicio 2 */
