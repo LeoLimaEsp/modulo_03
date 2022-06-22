53dba2baf757403b0b9175a0019c7b7dc6ee35b5
leonel <leonel.limae@hotmail.com>
2022-06-22 10:14:16 -0500

    Proyecto en desarrollo

--- PROYECTO/JavaScript.js
+++ PROYECTO/JavaScript.js
@@ -1,31 +1,42 @@
 let search_input = document.getElementById("search_input")
-let container = document.getElementById("container")
-
+let  = document.getElementById("container")
 let json = JSON.parse(file)
 let personajes = json.results
 
-container.innerHTML = personajes.name;
-function search() {
-  let personaje = search_personaje(search_input.value)
-  
-  container.innerHTML = `
-  <p>
-    Name: ${personaje.name}<br/>
-    Gender: ${personaje.gender}<br/>
-    Birth: ${personaje.birth_year}
-  </p>
-  `
 
+function fetchStarWars () {  // foreach dentro de funcion para poder ser llamada en diferentes partes del codigo
+  personajes.forEach(element => {
+    container.innerHTML+= loadCard(element.name,element.gender,element.birth_year, element.mass)
+  });
 }
 
-function search_personaje(personaje_name) {
-  for(let i = 0; i<personajes.length; i++) {
-    //console.log(personajes[i].name)
-    
-    if(personajes[i].name == personaje_name) {
-      return personajes[i]
-    }
-  }
-}
+fetchStarWars() // al cargar la pagina se llama esta funcion por si sola, mostrando todos los personajes
 
-console.log(personajes)
+search_input.addEventListener("keyup", (event) => {
+
+  container.innerHTML = "";
+  let searching = personajes.filter(({ name }) => name
+                                    .toLocaleLowerCase()
+                                    .includes(event.target.value
+                                    .toLocaleLowerCase()));
+                                    
+  searching.forEach(element =>container.innerHTML+= loadCard(element.name,element.gender,element.birth_year,  element.mass));
+
+  if(event.target.value === ""){ 
+    container.innerHTML = "";  //Limpia el container
+    fetchStarWars();  //si no escribio nada, cargar todos los personajes
+}
+});
+
+function loadCard(name,gender,birth_year,mass){
+  return `
+  <div class="card" style="width: 18rem;">
+      <img src="./img/${name}.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <h5 class="card-title"> Gender: ${gender}</h5>
+          <h5 class="card-title"> Birth-Year: ${birth_year}</h5>
+          <h5 class="card-title"> Mass: ${mass}</h5>
+      </div>
+  </div>`
+}
